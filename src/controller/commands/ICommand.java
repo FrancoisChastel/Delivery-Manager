95cc786e94b2375aed6c09c72eab5a81bb21e4bc
FrancoisChastel <francois@chastel.co>
2016-12-03 19:22:00 +0100

    Modify - Refactor name of package

--- src/controller/commands/ICommand.java
+++ src/controller/commands/ICommand.java
@@ -7,7 +7,11 @@
 package controller.commands;
 
 /**
+<<<<<<< 29fd2f6a18b2017c769c4def39a77583c7f1ced5
  * Command interface that is undoable 
+=======
+ * Command interface that is not undoable 
+>>>>>>> Modify - Refactor name of package
  * @author FACANT
  *
  */
