8dd315f164b01bbd6fbfc196acddd977938aedfc
FrancoisChastel <francois@chastel.co>
2016-12-03 19:54:17 +0100

    Modify - Manage undo historic

--- src/controller/commands/ICommander.java
+++ src/controller/commands/ICommander.java
@@ -18,6 +18,14 @@ public interface ICommander {
 	public List<ICommand> historic(CommandContext context, int size);
 	
 	/**
+	 * obtain a list of the last undo top command of the context
+	 * @param context of execution that need to be returned
+	 * @param number of commands that will be returned
+	 * @return last commands
+	 */
+	public List<ICommand> undoHistoric(CommandContext context, int size);
+	
+	/**
 	 * execute the command
 	 * @param context of execution of the command
 	 * @param command that will be executed
